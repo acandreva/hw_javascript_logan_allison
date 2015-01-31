9973cbca18b8653824ebc5f26181cec2fdd8e418
Allison Logan <agcandreva@yahoo.com>
2015-01-30 19:26:26 -0500

    adds inception function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
-var numba = function (string) {
-    if (string.length < 7) {
-        console.log("What a short little word!");
-    }
-    else if (string.length > 7) {
-        console.log("I'm sorry, but that's too many to count.");
-    }
-    else {
-        console.log("7, what a perfect choice!")
-    }
+function inception(callback,value) {
+    callback(value);
 }
 
-numba("7");
-numba("seventy");
-numba("university");
+function execute() {
+    console.log("The Wizard of Oz is a fantastic movie.");
+}
+
+inception(execute);
