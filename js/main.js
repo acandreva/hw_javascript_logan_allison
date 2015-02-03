69ed6e93b43118648cb3b5b985fdb927feb39bcc
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-03 09:37:47 -0500

    bring 'howdy' & 'numba' function back into JS file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+function howdy () {
+    console.log("Allison Logan says bonjour.")
+}
+
+howdy();
+
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log("What a short little word!");
+    }
+    else if (string.length > 7) {
+        console.log("I'm sorry, but that's too many to count.");
+    }
+    else {
+        console.log("7, what a perfect choice!")
+    }
+}
+
+numba("7");
+numba("seventy");
+numba("university");
+
+
 function inception(callback,value) {
     callback(value);
 }
